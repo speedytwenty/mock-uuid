2f9215154dd15904e2f267701663661f235d6c0f
speedytwenty <speedytwenty@gmail.com>
2022-04-11 16:27:34 -0600

    Recompile

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,10 @@
 export declare class mockUuid {
-    private static getHexNumber;
     static getIncrementalGenerator(seed?: number, uuidVersion?: number, variant?: number): () => string;
     static getGenerator(seed?: number, uuidVersion?: number, variant?: number): (i: number) => string;
     static get(i: number, seed?: number, uuidVersion?: number, variant?: number): string;
+    private static getHexNumber;
 }
+export declare const v2: () => string;
+export declare const v3: () => string;
+export declare const v4: () => string;
+export declare const v5: () => string;
